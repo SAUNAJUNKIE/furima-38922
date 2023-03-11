11a40e5865ef0194731f515d749c920067b15a8a
SAUNAJUNKIE <hijiri0420@gmail.com>
2023-03-11 15:52:58 +0900

    Revert "商品消去機能の実装"
    This reverts commit c9a8368ecc83a75d4b89616918cc2eed3aca481e.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
 
@@ -45,14 +45,7 @@ end
     end
   end
   
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      render :show
-  end
-end
-
+  
   
   
   private
