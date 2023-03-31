352e35f25c09edc1d1a8a37170d9c184c93e820d
SAUNAJUNKIE <hijiri0420@gmail.com>
2023-03-31 17:12:19 +0900

    半角スペースの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,6 @@ validates :birthday, presence: true
   has_many  :items
   has_many :orders
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+ 
 end
 
